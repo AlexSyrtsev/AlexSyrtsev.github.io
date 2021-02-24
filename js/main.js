b87f2b35a3a41c27ed7144e78d5ead5d34f07ce0
Алексей Сырцев <79574158+AlexSyrtsev@users.noreply.github.com>
2021-02-24 13:28:30 +0300

    spa
    spa template

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.container-aboutus').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    speed: 1000,
+    dots: true,
+    
+    // prevArrow: $('.prev'),
+    // nextArrow: $('.next'),
+});
+
+$('.container-testimonial').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    dots: true,
+    
+    // prevArrow: $('.prev'),
+    // nextArrow: $('.next'),
+});
+
+// $('.container-aboutus').slick-prev({
+//     left: 100px;
+//     background: red; /*to notice it, is white*/
+// });
+// $('.container-aboutus').slick-next({
+//     right: 100px;
+//     background: red; /*to notice it, is white*/
+// });
